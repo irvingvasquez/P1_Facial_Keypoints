62fea6c0d3dc8308536621879a3c0141d3286507
Irving Vasquez <jirvingvg@gmail.com>
2018-11-26 17:58:32 -0600

    project completed

--- models.py
+++ models.py
@@ -20,35 +20,32 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
+        
+        # I implemented a varian of the network proposed by Agarwal [Facial Key Points Detection using Deep Convolutional Neural Network - NaimishNet]
+        
+        # Four convolutional layers with max pooling
+        
         self.conv1 = nn.Conv2d( 1, 32, 7)
         self.pool1 = nn.MaxPool2d(4, 4)
-        #self.drop1 = nn.Dropout(dropout_prob)
         
         self.conv2 = nn.Conv2d(32, 64, 5)
         self.pool2 = nn.MaxPool2d(2, 2)
-        #self.drop2 = nn.Dropout(dropout_prob)
         
         self.conv3 = nn.Conv2d(64, 128, 2)
         self.pool3 = nn.MaxPool2d(2, 2)
-        #self.drop3 = nn.Dropout(dropout_prob)
               
         self.conv4 = nn.Conv2d(128,256, 1)
         self.pool4 = nn.MaxPool2d(2, 2)
-        #self.drop4 = nn.Dropout(dropout_prob)
-         
+        
+        # Three fully connected layers
+        
         self.fc1 = nn.Linear(9216, 5000)   
-        #self.fc1 = nn.Linear(43264, 8652)
-        #self.fc1 = nn.Linear(12544, 8652)
-        #self.fc1 = nn.Linear(6400, 1000)
         self.drop5 = nn.Dropout(dropout_prob)
         
         self.fc2 = nn.Linear(5000, 1024)
-        #self.fc2 = nn.Linear(10000, 1000)
         self.drop6 = nn.Dropout(dropout_prob)
         
         self.fc3 = nn.Linear(1024, 136)
-        #self.fc3 = nn.Linear(1000, 136)
-        
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
@@ -60,20 +57,15 @@ class Net(nn.Module):
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.selu(self.conv1(x)))
         x = self.pool1(F.selu(self.conv1(x)))
-        #x = self.drop1(x)
         
         x = self.pool2(F.selu(self.conv2(x)))
-        #x = self.drop2(x)
         
         x = self.pool3(F.selu(self.conv3(x)))
-        #x = self.drop3(x)
         
         x = self.pool4(F.selu(self.conv4(x)))
-        #x = self.drop4(x)
         
         # Aplanar
         x = x.view(x.size(0), -1)
-        #print(x.shape)
         
         x =  F.selu(self.fc1(x))
         x =  self.drop5(x)
